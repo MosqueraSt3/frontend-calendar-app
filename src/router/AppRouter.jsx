5ca945618317d5347c528828acfcb82737d7c2b5
mxsqxxrx <mosquerast3.dev@gmail.com>
2023-01-10 10:19:29 -0500

    variables

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,10 +2,11 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { LoginPage } from '../auth';
 import { CalendarPage } from '../calendar';
+import { getEnvVariables } from '../helpers';
 
 export const AppRouter = () => {
 
-    const authStates = 'authenticated';
+    const authStates = 'not-authenticated';
 
     return (
         <Routes>
